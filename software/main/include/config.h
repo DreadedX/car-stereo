f57131d9844f82d4436a5c474e11914caebc156d
Dreaded_X <tim@huizinga.dev>
2022-09-29 03:18:35 +0200

    Added pin config for production

--- software/main/include/config.h
+++ software/main/include/config.h
@@ -2,8 +2,15 @@
 
 #include "hal/gpio_types.h"
 
-#define I2S_PIN_BCK GPIO_NUM_26
-#define I2S_PIN_WS GPIO_NUM_25
-#define I2S_PIN_DATA GPIO_NUM_33
-#define TWAI_PIN_CTX GPIO_NUM_5
-#define TWAI_PIN_CRX GPIO_NUM_19
+// FOR PROTOTYPE
+/* #define I2S_PIN_BCK GPIO_NUM_26 */
+/* #define I2S_PIN_WS GPIO_NUM_25 */
+/* #define I2S_PIN_DATA GPIO_NUM_33 */
+/* #define TWAI_PIN_CTX GPIO_NUM_5 */
+/* #define TWAI_PIN_CRX GPIO_NUM_19 */
+
+#define I2S_PIN_BCK GPIO_NUM_5
+#define I2S_PIN_WS GPIO_NUM_16
+#define I2S_PIN_DATA GPIO_NUM_17
+#define TWAI_PIN_CTX GPIO_NUM_32
+#define TWAI_PIN_CRX GPIO_NUM_35
