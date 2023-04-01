987f778a25c16d5bf62afd830df878e7563e33f5
Dreaded_X <tim@huizinga.dev>
2023-04-01 04:10:57 +0200

    Added menuconfig option to switch between prototype and eventual production hardware

--- software/main/include/config.h
+++ software/main/include/config.h
@@ -3,14 +3,19 @@
 #include "hal/gpio_types.h"
 
 // FOR PROTOTYPE
-/* #define I2S_PIN_BCK GPIO_NUM_26 */
-/* #define I2S_PIN_WS GPIO_NUM_25 */
-/* #define I2S_PIN_DATA GPIO_NUM_33 */
-/* #define TWAI_PIN_CTX GPIO_NUM_5 */
-/* #define TWAI_PIN_CRX GPIO_NUM_19 */
+#ifdef CONFIG_CAR_STEREO_PROTOTYPE
+	#pragma message ( "Building for the prototype" )
+	#define I2S_PIN_BCK GPIO_NUM_26
+	#define I2S_PIN_WS GPIO_NUM_25
+	#define I2S_PIN_DATA GPIO_NUM_33
+	#define TWAI_PIN_CTX GPIO_NUM_5
+	#define TWAI_PIN_CRX GPIO_NUM_19
+#else
+	#pragma message ( "Building for the production" )
+	#define I2S_PIN_BCK GPIO_NUM_5
+	#define I2S_PIN_WS GPIO_NUM_16
+	#define I2S_PIN_DATA GPIO_NUM_17
+	#define TWAI_PIN_CTX GPIO_NUM_32
+	#define TWAI_PIN_CRX GPIO_NUM_35
+#endif
 
-#define I2S_PIN_BCK GPIO_NUM_5
-#define I2S_PIN_WS GPIO_NUM_16
-#define I2S_PIN_DATA GPIO_NUM_17
-#define TWAI_PIN_CTX GPIO_NUM_32
-#define TWAI_PIN_CRX GPIO_NUM_35
